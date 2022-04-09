43ca5a79181bab2f82bc1f0a13b0292daf2c4a59
huaqi <2931246464@qq.com>
2022-04-09 16:22:55 +0800

    优化请求习题部分代码

--- POYI/miniprogram/pages/topicList/topicList.js
+++ POYI/miniprogram/pages/topicList/topicList.js
@@ -40,47 +40,37 @@ Page({
         wx.showLoading({
             title: '生成习题中~',
         })
+
         let type = this.data.type;
         const typeCut = type.slice(0, type.length - 1);
         const reg = /[4,6,8]+/g;
+        const sign = this.data.part.substring(5, 8);
         const part = this.data.part.replace(reg, "");
-        const sign = this.data.part.substring(5,8);
         const id = e.currentTarget.dataset.id;
-        
+
         // 请求整套习题
         if (sign === "ALL") {
-            let partName = "";
-            let partArray = "";
-            if (type === "CET4" || type === "CET6") {
-                partName = "writing";
-                partArray = ["CET_writing", "CET_listening", "CET_reading", "CET_translation"];
-            } else if (type === "TEM4") {
-                partName = "listening";
-                partArray = ["TEM_listening", "TEM_cloze", "TEM_languageUsage", "TEM_reading", "TEM_writing"];
-            } else if (type === "TEM8") {
-                partName = "listening";
-                partArray = ["TEM_listening", "TEM_reading", "TEM_languageUsaage", "TEM_translation", "TEM_writing"];
-            }
 
-            for (let item of partArray) {
-                await this.getExercisePart({
-                    part: item,
-                    id,
-                    type
-                })
-            }
-           
+            let partName = await this.getAllExercises({
+                id,
+                type
+            });
+
             wx.hideLoading();
             wx.redirectTo({
                 url: `/pages/exercise/exercise?sign=all&part=${typeCut}_${partName}&type=${type}`,
             })
+
         } else {
-           
-            await this.getExercisePart({
+
+            let res = await this.getExercisePart({
                 part,
                 id,
                 type
             });
+
+            wx.setStorageSync(`exercise_${part.slice(4)}`, res.data[0]);
+
             wx.hideLoading();
             wx.redirectTo({
                 url: `/pages/exercise/exercise?part=${part}&type=${type}`
@@ -89,23 +79,60 @@ Page({
 
     },
 
-    // 请求 习题函数
-    async getExercisePart(event) {
+    // 性能优化 请求 习题函数
+    getExercisePart(event) {
         const {
             part,
             id,
             type
-        } = event;
-        console.log(event)
-        await db.collection(part).where({
-            type,
-            id
-        }).get().then(res => {
-            console.log(res);
-            wx.setStorageSync(`exercise_${part.slice(4)}`, res.data[0]);
-        });
+        } = event
+
+        return new Promise((resolve, reject) => {
+            db.collection(part).where({
+                type,
+                id
+            }).get().then(res => {
+                resolve(res);
+            }).catch(err => {
+                reject(err);
+            })
+        })
     },
 
+    async getAllExercises(event) {
+        let {
+            id,
+            type
+        } = event;
+        let partName, partArray;
 
+        if (type === "CET4" || type === "CET6") {
+            partName = "writing";
+            partArray = ["CET_writing", "CET_listening", "CET_reading", "CET_translation"];
+        } else if (type === "TEM4") {
+            partName = "listening";
+            partArray = ["TEM_listening", "TEM_cloze", "TEM_languageUsage", "TEM_reading", "TEM_writing"];
+        } else if (type === "TEM8") {
+            partName = "listening";
+            partArray = ["TEM_listening", "TEM_reading", "TEM_languageUsaage", "TEM_translation", "TEM_writing"];
+        }
+
+        let getExercisePromises = partArray.map(part => this.getExercisePart({
+            part,
+            id,
+            type
+        }))
+
+        let allExercise = await Promise.all(getExercisePromises);
+
+        allExercise.forEach((item, index) => {
+            wx.setStorageSync(
+                `exercise_${partArray[index].slice(4)}`,
+                item.data[0]
+            );
+        })
+
+        return partName;
+    }
 
 })
