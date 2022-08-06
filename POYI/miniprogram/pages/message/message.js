69f9bed6658b6139f61f7ff27fa19ef1f9c8ad7b
huaqi <2931246464@qq.com>
2022-08-06 14:54:02 +0800

    优化message组件布局与逻辑-添加组件按需加载

--- POYI/miniprogram/pages/message/message.js
+++ POYI/miniprogram/pages/message/message.js
@@ -5,6 +5,8 @@ const openId = wx.getStorageSync('openId');
 
 Page({
   data: {
+    // 滚动栏默认位置
+    viewId: "",
     message: "",
     messagesList: [],
     openId: "",
@@ -12,50 +14,58 @@ Page({
   },
 
   onLoad() {
+    overallMessagesList = [];
     this.getMessagesCollection();
+    // 加载即显示最后一条信息
+    this.setData({
+      openId: openId,
+      viewId: "msg-5"
+    })
   },
- 
+
 
   // 获取留言
-  getMessagesCollection(i = 0) {
+  getMessagesCollection(skipIndex = 0, limitNum = 6) {
     // 获取数据
     db.collection("messages")
       .orderBy("timeStamp", "desc")
-      .skip(6 * i)
-      .limit(6)
+      .skip(6 * skipIndex)
+      .limit(limitNum)
       .get()
       .then(res => {
         overallMessagesList.push(...res.data);
         // 升序排序
-        // overallMessagesList.sort(this.sortBy('timeStamp', 1))
         overallMessagesList.sort((firstEl, secondEl) => firstEl.timeStamp - secondEl.timeStamp);
+
         this.setData({
           messagesList: overallMessagesList,
-          openId
+          // 根据 limitNum 判断是下拉刷新还是输入
+          viewId: `msg-${limitNum === 6 ? 5 : overallMessagesList.length - 1}`
         })
       })
-  },
-
-  // 获取留言信息
-  getMessage(e) {
-    console.log(e);
-    const message = e.detail.value;
-    this.setData({
-      message
-    })
   },
 
   // 下拉获取更多信息
   dropDown() {
     // 获取messages
     this.getMessagesCollection(overallLength++);
-    this.dropDownStop();
+    this.dropDownStop(); 
   },
 
   // 停止下拉刷新
   dropDownStop() {
     this.setData({
       triggered: false
+    })
+  },
+
+
+  // 用户输入留言
+  getMessage(e) {
+    console.log(e);
+    const message = e.detail.value;
+    this.setData({
+      message
     })
   },
 
@@ -70,17 +80,18 @@ Page({
         duration: 2000
       })
       return
-    } else {
-      this.addMessage(this.data.message);
     }
-    
+
+    this.addMessage(this.data.message);
+
   },
 
-  // 同步数据库
+  // 数据库添加信息
   addMessage(message) {
     // 获取用户名
     const {
-      nickName
+      nickName,
+      avatarUrl
     } = wx.getStorageSync('userInfo');
     // 获取当前时间戳
     const timeStamp = Date.now();
@@ -88,21 +99,17 @@ Page({
         // data 字段表示需新增的 JSON 数据
         data: {
           nickName,
+          avatarUrl,
           message,
           timeStamp
         }
       })
       .then(res => {
-        console.log(res);
         // 这里是重点
-        // 为了达到每次发送留言后，页面总显示最新的 6 条数据效果
-        // 特此添加了一个全局数组，每次重新请求最新信息时，进行清空
-        overallMessagesList = [];
         this.setData({
-          message: ""
+          message: "",
         })
-        // 重新获取 message 信息
-        this.getMessagesCollection();
+        this.getMessagesCollection(0, 1);
       })
   }
 })
